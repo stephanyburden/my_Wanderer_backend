3cdc859509a424eb0c4fe2829e67401bf9225403
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-02 15:07:33 -0400

    created controller

---
+++ Backend/controllers/cityController.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const db = require('../models');
+
+//index route --> return data for all cities
+router.get('/', (req, res) => {
+    db.City.find({}, (err, foundCity) => {
+        if (err) return console.log(err);
+        res.json(foundCity)
+    })
+})
+//show route --> data for one city
+router.get('/:id', (req,res) => {
+    db.City.findById(req.params.id, (err,foundCity) => {
+        if (err) return console.log(err);
+
+        res.json(foundCity);
+    });
+})
+
+
+//post route --> create a city
+router.post('/', (req, res) => {
+    db.City.create(req.body, (err, savedCity) => {
+      if (err) return console.log(err);
+      
+      res.json(savedCity);
+    });
+  });
+
+
+//update route --> update a city
+router.put('/:id', (req, res) => {
+    db.City.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {new: true},
+      (err, updatedCity) => { // 
+        if (err) return console.log(err);
+        
+        res.json(updatedCity);
+      });
+  });
+
+
+
+//delete route --> delete a city
+router.delete('/:id', (req, res) => {
+    db.City.findByIdAndDelete(req.params.id, (err, deletedCity) => {
+        if (err) return console.log(err);
+        res.json("We deleted this")
+    })
+})
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 // app.use(cors())
 // app.use(express.json())
 
-// app.use('/api/city', cityController)
+app.use('/api/city', cityController)
 
 app.listen(port, () => {
     console.log(`Brb, journeying across port ${port}`)
