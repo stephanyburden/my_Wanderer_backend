73f4fe68323f19fe696de5243f3424c0c7b6d8a8
nndeto <86444679+nndeto@users.noreply.github.com>
2021-09-02 22:44:29 -0400

    added notes to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express'); //calling in express our middleware middleman
+const cors = require('cors'); //the calling card between our front end and back end
+const rowdy = require('rowdy-logger') //helps us visually see our routes
+
+
+
+//DB and Models
 const cityController = require('./controllers/cityController.js')
 const postController = require('./controllers/postController.js')
 
+
+//Configuration
 const port = 4000;
 const app = express();
-
+const rowdyResults = rowdy.begin(app)
 app.use(cors())
+
+
 // allows for req.body
 app.use(express.json()) 
+//will need below later for sessions
+//app.use(sesson({secret}))
 
+
+//Controllers --> our url will be localhost:4000/api/city or /api/post
 app.use('/api/city', cityController)
-app.use('/api/city', postController)
+app.use('/api/post', postController)
 
 
 app.listen(port, () => {
